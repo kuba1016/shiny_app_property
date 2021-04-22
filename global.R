52cb8de48288d5a4c43d762463c2eb37417d5411
Jakub Kozanowski <kuba1016@gmail.com>
2021-04-22 13:06:55 +0100

    encoding issue for voivodeship signs resolved

--- global.R
+++ global.R
@@ -7,7 +7,8 @@ library(bslib)
 # loding data.
 new_homes <- vroom("data/new_homes.csv") %>%
   select(-url) %>%
-  filter(m2 > 20)
+  filter(m2 > 20) %>% 
+  mutate(voivodeship = str_trim(voivodeship))
 old_homes <- vroom("data/old_homes.csv") %>%
   select(-url) %>%
   filter(m2 > 20)
--- server.R
+++ server.R
@@ -14,6 +14,7 @@ server <- function(input, output) {
   data <- reactive(
     if ("All" %in% input$voiv) {
       homes_df()
+      
     } else {
       homes_df() %>% filter(voivodeship == input$voiv)
     }
@@ -76,7 +77,7 @@ server <- function(input, output) {
     {
       # outfile <- tempfile(fileext = ".png")
       list(
-        src = file.path(paste0("www/img/", input$voiv, ".svg")),
+        src = file.path(list.files("./www/img",pattern =paste0("^",stringi::stri_trans_nfd(input$voiv),"(.*)","$"),full.names = T)),
         alt = "This is sign of voivodship",
         width = 100,
         height = 100
